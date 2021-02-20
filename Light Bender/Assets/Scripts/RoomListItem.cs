a7a5b0f071bfa0285d72e6e785395cfa90a1f82e
ARNAUD <arnaud.lecoq@epita.fr>
2021-02-20 13:36:01 +0100

    BASE
    BASE

---
+++ Light Bender/Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class RoomListItem : MonoBehaviour
+{
+
+    [SerializeField] TMP_Text text;
+    private RoomInfo info;
+    public void Setup(RoomInfo _info )
+    {
+        info = _info;
+        text.text = info.Name;
+    }
+
+    public void OnClick()
+    {
+        Launcher.Instance.JoinRoom(info);
+    }
+}
