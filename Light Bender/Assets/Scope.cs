f498ee3bfeb5d4de303f2a71602ca524bc458edb
ARNAUD <arnaud.lecoq@epita.fr>
2021-04-22 09:48:24 +0200

    CORRECTIONS VISEUR + prefab

--- Light Bender/Assets/Scope.cs
+++ Light Bender/Assets/Scope.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon;
+using Photon.Pun;
 
 public class Scope : MonoBehaviour
 {
-   public Animator animator;
+   private  Animator animator;
+
+    private PhotonView Phv;
 
    private bool isScoped = false;
 
@@ -18,23 +23,36 @@ public class Scope : MonoBehaviour
 
    private float normalFOV;
 
-   void Update()
+   private void Awake()
+   {
+      Phv = GetComponent<PhotonView>();
+   }
+
+   private void Start()
+   {
+      if (Phv.IsMine)
+         animator = GetComponent<Animator>();
+   }
+
+   private void Update()
    {
       if (Input.GetButtonDown("Fire2"))
       {
-         isScoped = !isScoped;
-         animator.SetBool("Scoped",isScoped);
-         scopeOverlay.SetActive(isScoped);
-         if (isScoped)
-            StartCoroutine(OnScoped());
-         else
-            OnUnscoped();
+         
+         if (!Phv.IsMine)
+            return;
+         {
+            isScoped = !isScoped;
+            animator.SetBool("Scoped",isScoped);
+            scopeOverlay.SetActive(isScoped);
+            if (isScoped)
+               StartCoroutine(OnScoped());
+            else
+               OnUnscoped();
+         }
+         
       }
 
-     
-      
-         
-      
    }
 
    IEnumerator OnScoped()
@@ -48,7 +66,7 @@ public class Scope : MonoBehaviour
       mainCamera.fieldOfView = scopedFOV;
    }
 
-   void OnUnscoped()
+   private void OnUnscoped()
    {
       scopeOverlay.SetActive(false);
       weaponCam.SetActive(true);
